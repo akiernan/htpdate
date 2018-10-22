a26596392063088395f7fd7b50b23921623781c5
Alex Kiernan <alex.kiernan@hivehome.com>
2018-10-22 09:13:13 +0100

    htpdate-1.0.4

--- htpdate.c
+++ htpdate.c
@@ -1,5 +1,5 @@
 /*
-	htpdate v1.0.3
+	htpdate v1.0.4
 
 	Eddy Vervest <eddy@cleVervest.com>
 	http://www.clevervest.com/htp
@@ -52,7 +52,7 @@
 #include <pwd.h>
 #include <grp.h>
 
-#define VERSION 				"1.0.3"
+#define VERSION 				"1.0.4"
 #define	MAX_HTTP_HOSTS			15				/* 16 web servers */
 #define	DEFAULT_HTTP_PORT		"80"
 #define	DEFAULT_PROXY_PORT		"8080"
@@ -209,6 +209,8 @@ static long getHTTPdate( char *host, char *port, char *proxy, char *proxyport, c
 
 		break;
 	} while ( ( res = res->ai_next ) );
+
+	freeaddrinfo(res0);
 
 	if ( rc ) {
 		printlog( 1, "%s connection failed", host );
--- htpdate.spec
+++ htpdate.spec
@@ -1,6 +1,6 @@
 Summary: HTTP based time synchronization tool
 Name: htpdate
-Version: 1.0.3
+Version: 1.0.4
 Release: 1
 License: GPL
 Group: System Environment/Daemons
